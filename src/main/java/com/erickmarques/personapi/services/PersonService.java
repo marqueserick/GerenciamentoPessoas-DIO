f2de14a961c45d7ebc94b0afddc78fcc114eba2c
Erick Marques <erickmarques43@gmail.com>
2021-09-05 15:15:12 -0300

    add listAll method

--- src/main/java/com/erickmarques/personapi/services/PersonService.java
+++ src/main/java/com/erickmarques/personapi/services/PersonService.java
@@ -5,9 +5,12 @@ import com.erickmarques.personapi.dto.request.PersonDTO;
 import com.erickmarques.personapi.dto.response.MessageResponseDTO;
 import com.erickmarques.personapi.entities.Person;
 import com.erickmarques.personapi.repositories.PersonRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -26,5 +29,10 @@ public class PersonService {
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder()
                 .message("Created person with ID"+savedPerson.getId()).build();
+    }
+
+    public List<PersonDTO> listAll(){
+        List<Person> people = personRepository.findAll();
+        return people.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
